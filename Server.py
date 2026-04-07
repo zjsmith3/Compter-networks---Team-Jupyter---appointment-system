9c18e7fb1f08e9bea95ef202c3f216d3cc569482
zjsmith3 <143528675+zjsmith3@users.noreply.github.com>
2026-04-07 20:20:59 +0000

    adding state function skeletons, and created some helper functions recieve and send json so that I won't have to rewrite those parts over and over again.

--- Server.py
+++ Server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import json 
 
 HOST = "127.0.0.1"
 PORT = 5000
@@ -29,12 +30,79 @@ def startServer():
         )
         
 
-def handleClient(scslientSocket, clientAddress):
+def handleClient(clientSocket, clientAddress):
     session = {
-        "state": "START",
-        "selectedDoctor": None,
-        "selectedDate": None
+        "state": "START",  #the state the client is currently in
+        "selectedDoctor": None,  #memory spot for chosen doctor for fetch function purposes
+        "selectedMonth": None,  #memory spot for chosen month for fetch function purposes
+        "selectedDate": None #memory spot for chosen date for fetch function purposes
     }
+
+    try:
+        while True:
+            state = session["state"]
+
+            #depending on the current state of the program, it will call that function
+            if state == "START":
+                startState(clientSocket, session)
+            elif state == "DOCTOR":
+                chooseDoctor(clientSocket, session)
+            elif state == "MONTH":
+                chooseMonth(clientSocket, session)
+            elif state == "DAY":
+                chooseDay(clientSocket, session)
+            elif state == "TIME":
+                chooseTimeSlot(clientSocket, session)
+            elif state == "EXIT":
+                break
+            else:
+                break
+    
+    finally:
+        clientSocket.close()
+
+
+#This is a function used for sending json files to the client
+def sendJson(clientSocket, messageFile):
+    message = json.dumps(messageFile) + "\n"
+    clientSocket.sendall(message.encode())
+
+#and this function is for receiving messages
+def receiveJson(clientSocket):
+    data = b""
+
+    while b"\n" not in data:
+        #this loop takes in chunks of data untill the end of the message. 
+        chunk = clientSocket.recv(4096)
+        if not chunk:
+            return None
+        data += chunk
+    
+    message = data.decode().strip()
+    return json.loads(message)
+
+def startState(clientSocket, session):
+    print("temp")
+
+
+def chooseDoctor(clientSocket, session):
+    print("temp")
+
+
+def chooseMonth(clientSocket, session):
+    print("temp")
+
+
+def chooseDay(clientSocket, session):
+    print("temp")
+
+
+def chooseTimeSlot(clientSocket, session):
+    print("temp")
+
+
+
+
 
 if __name__ == "__main__":
     startServer()
