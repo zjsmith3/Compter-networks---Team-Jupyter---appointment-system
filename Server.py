e9ca9c977670019987493cbd045001c06da786f4
zjsmith3 <143528675+zjsmith3@users.noreply.github.com>
2026-04-08 20:18:30 +0000

    copy and pasted chooseDoctor code into month, day, function with small edits for naming conventions and workflow. Also created the choose time slot. in total work flow is layed out. All that's left is to wait for william's functions.

--- Server.py
+++ Server.py
@@ -2,6 +2,9 @@ import socket
 import threading
 import json 
 
+#this is a mutex lock (learned about it in operating systems). It's the tool used to stop race conditions. 
+bookingLock = threading.Lock()
+
 HOST = "127.0.0.1"
 PORT = 5000
 
@@ -70,8 +73,8 @@ def handleClient(clientSocket, clientAddress):
 def sendJson(clientSocket, messageFile):
     message = json.dumps(messageFile) + "\n"
     clientSocket.sendall(message.encode())
-#and this function is for receiving messages
 
+#and this function is for receiving messages
 def receiveJson(clientSocket):
     data = b""
 
@@ -157,14 +160,15 @@ def chooseDoctor(clientSocket, session, status="OK"):
     if request == None:
         session["state"] = "EXIT"
         return
-
-    #this makes the variable choice the client's response.
-    choice = request.get("choice")
     
     #this is for if the client wants to go back a step (in this case to the main menu) I made it an "action" rather than choice to avoid mistakes.
     if request.get("action") == "BACK":
         session["state"] = "START"
         return
+
+    #this makes the variable choice the client's response.
+    choice = request.get("choice")
+    
 
     if not isinstance(choice, int):
         chooseDoctor(clientSocket, session, status="INPUT_ERROR")
@@ -179,16 +183,202 @@ def chooseDoctor(clientSocket, session, status="OK"):
     chooseDoctor(clientSocket, session, status="INPUT_ERROR")
     
 
-def chooseMonth(clientSocket, session):
-    print("temp")
+def chooseMonth(clientSocket, session, status="OK"):
+    #this will be where the fetch function for the list of doctors will happen, for now I use temp list
+    months = temporaryMonths
 
-def chooseDay(clientSocket, session):
-    print("temp")
+    #gonna take the list, and append the list to have numbers for choosing. 
+    options = []
+    for i, month in enumerate(months, start=1):
+        options.append({
+            "id": i,
+            "name": month
+        })
+    
 
-def chooseTimeSlot(clientSocket, session):
-    print("temp")
+
+    #determining prompt based off status.
+    if status == "INPUT_ERROR":
+        prompt = "Input invalid, try again. Choose a month to book in"
+    else:
+        prompt = "Choose a month"
+
+    #send the info for this stage
+    sendJson(clientSocket, messageFile= {
+        "status": status, 
+        "state": "MONTH",
+        "prompt": prompt,
+        "options": options,
+        "back": True
+    })
+
+    #get response
+    request = receiveJson(clientSocket)
+
+    #if the client doesn't respond, assume to exit for safety reasons
+    if request == None:
+        session["state"] = "EXIT"
+        return
+
+    #this is for if the client wants to go back a step (in this case to the main menu) I made it an "action" rather than choice to avoid mistakes.
+    if request.get("action") == "BACK":
+        session["state"] = "DOCTOR"
+        return
+    
+    #this makes the variable choice the client's response.
+    choice = request.get("choice")
+    
+
+    if not isinstance(choice, int):
+        chooseMonth(clientSocket, session, status="INPUT_ERROR")
+        return
+
+    if 1 <= choice <= len(months):
+        selected = months[choice - 1]
+        session["selectedMonth"] = selected
+        session["state"] = "DAY"
+        return
+
+    chooseMonth(clientSocket, session, status="INPUT_ERROR")
+
+def chooseDay(clientSocket, session, status="OK"):
+    #this will be where the fetch function for the list of doctors will happen, for now I use temp list
+    days = temporaryDays
+
+    #gonna take the list, and append the list to have numbers for choosing. 
+    options = []
+    for i, day in enumerate(days, start=1):
+        options.append({
+            "id": i,
+            "name": day
+        })
+    
 
 
+    #determining prompt based off status.
+    if status == "INPUT_ERROR":
+        prompt = "Input invalid, try again. Choose a day"
+    else:
+        prompt = "Choose a day"
+
+    #send the info for this stage
+    sendJson(clientSocket, messageFile= {
+        "status": status, 
+        "state": "DAY",
+        "prompt": prompt,
+        "options": options,
+        "back": True
+    })
+
+    #get response
+    request = receiveJson(clientSocket)
+
+    #if the client doesn't respond, assume to exit for safety reasons
+    if request == None:
+        session["state"] = "EXIT"
+        return
+
+    #this is for if the client wants to go back a step (in this case to the main menu) I made it an "action" rather than choice to avoid mistakes.
+    if request.get("action") == "BACK":
+        session["state"] = "MONTH"
+        return
+
+    #this makes the variable choice the client's response.
+    choice = request.get("choice")
+
+    if not isinstance(choice, int):
+        chooseDay(clientSocket, session, status="INPUT_ERROR")
+        return
+
+    if 1 <= choice <= len(days):
+        selected = days[choice - 1]
+        session["selectedDay"] = selected
+        session["state"] = "TIME"
+        return
+
+    chooseDay(clientSocket, session, status="INPUT_ERROR")
+
+def chooseTimeSlot(clientSocket, session, status="OK"):
+    #this will be where the fetch function for the list of doctors will happen, for now I use temp list
+    timeSlots = temporaryTimeSlots
+
+    #gonna take the list, and append the list to have numbers for choosing. 
+    options = []
+    for i, slot in enumerate(timeSlots, start=1):
+        options.append({
+            "id": i,
+            "name": slot
+        })
+    
+
+
+    #determining prompt based off status.
+    if status == "INPUT_ERROR":
+        prompt = "Input invalid, try again. Choose a time slot and enter your info."
+    elif status == "TAKEN":
+        prompt = "That time slot was just taken. Choose another time slot."
+    else:
+        prompt = "Choose a time slot and entor your info"
+
+    #send the info for this stage
+    sendJson(clientSocket, messageFile= {
+        "status": status, 
+        "state": "TIME",
+        "prompt": prompt,
+        "options": options,
+        "back": True
+    })
+
+    #get response
+    request = receiveJson(clientSocket)
+
+    #if the client doesn't respond, assume to exit for safety reasons
+    if request == None:
+        session["state"] = "EXIT"
+        return
+
+    #this is for if the client wants to go back a step (in this case to the main menu) I made it an "action" rather than choice to avoid mistakes.
+    if request.get("action") == "BACK":
+        session["state"] = "DAY"
+        return
+    
+    #this makes the variable choice the client's response.
+    choice = request.get("choice")
+
+    if not isinstance(choice, int):
+        chooseTimeSlot(clientSocket, session, status="INPUT_ERROR")
+        return
+
+    if not (1 <= choice <= len(timeSlots)):
+        chooseTimeSlot(clientSocket, session, status="INPUT_ERROR")
+        return
+    
+    name = request.get("name")
+    email = request.get("email")
+    reason = request.get("reason")
+
+    if not isinstance(name, str) or not name.strip():
+        chooseTimeSlot(clientSocket, session, status="INPUT_ERROR")
+        return
+
+    if not isinstance(email, str) or not email.strip():
+        chooseTimeSlot(clientSocket, session, status="INPUT_ERROR")
+        return
+
+    if not isinstance(reason, str) or not reason.strip():
+        chooseTimeSlot(clientSocket, session, status="INPUT_ERROR")
+        return
+
+    chosenTime = timeSlots[choice -1]
+
+    with bookingLock:
+        #this is where the booking of the time slot actually happens, can't do it without william's read/write functions
+        #but here's the work flow:
+        #check again if the chosen time slot is still available
+        #if it's free call the write function that gives name, email, reason, and officially books the time slot. 
+        #I think angel is doing an email confirmation function? so probably call that here as well. 
+
+        pass
 
 
 
